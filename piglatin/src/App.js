e2f510afccb45cfd8f280d7d044af0118fc33962
janwrx <janzosa@gmail.com>
2018-10-15 13:59:39 -0700

    piglatenafter

--- piglatin/src/App.js
+++ piglatin/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import piglatin from './piglatin';
+import Piglatin from './piglatin';
 import Input from './Input';
 
 class App extends Component {
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <h1>Inputs and Outputs</h1>
         <Input submit={this.takeInput.bind(this)}/>
-        <piglatin greeting={this.state.greeting}/>
+        <Piglatin greeting={this.state.greeting}/>
       </div>
     );
   }
